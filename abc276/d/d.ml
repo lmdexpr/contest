2a57946a82cd0bf6e31e52f3de72cdf69c97995d
lmdexpr <lmdexpr@gmail.com>
2022-11-07 18:33:13 +0900

    276 d to e

--- abc276/d/d.ml
+++ abc276/d/d.ml
@@ -1,3 +1,24 @@
-let solve () = 42
+open Core
 
-let () = print_int @@ solve ()
+let n = Scanf.scanf "%d" ident
+
+let a = List.init n ~f:(fun _ -> Scanf.scanf " %d" ident)
+
+let rec gcd a b = if b = 0 then a else gcd b (a mod b)
+let gcd = List.reduce_exn a ~f:gcd
+
+let a = List.map a ~f:(fun e -> e / gcd)
+
+let rec f ?(c=0) n =
+  if n = 1 then c
+  else if n % 2 = 0 then f ~c:(c+1) (n / 2)
+  else if n % 3 = 0 then f ~c:(c+1) (n / 3)
+  else
+    -1
+
+let a = List.map a ~f
+
+let () =
+  if List.exists a ~f:(fun e -> e = -1) then print_endline "-1"
+  else
+    printf "%d\n" @@ List.sum (module Int) a ~f:ident
--- abc276/e/e.ml
+++ abc276/e/e.ml
@@ -1,3 +1,51 @@
-let solve () = 42
+open Core
 
-let () = print_int @@ solve ()
+let h, w = Scanf.scanf "%d %d" @@ fun a b -> a, b
+
+let sx = ref 0
+let sy = ref 0
+
+let c = Array.init h ~f:(fun i ->
+    let s = Scanf.scanf " %s" ident in
+    for j = 0 to w - 1 do
+      if Char.equal s.[j] 'S' then begin
+        sx := i;
+        sy := j
+      end
+    done;
+    s
+  )
+
+let is_valid (x, y) = 0 <= x && x < h && 0 <= y && y < w
+
+let around = List.filter ~f:is_valid [
+  !sx + 1, !sy;
+  !sx - 1, !sy;
+  !sx, !sy + 1;
+  !sx, !sy - 1;
+]
+
+let uf = Array.init h ~f:(fun i -> Array.init w ~f:(fun j -> Union_find.create (i, j)))
+
+let () =
+  for i = 0 to h - 1 do
+    for j = 0 to w - 1 do
+      if Char.equal c.(i).[j] '.' then begin
+        if i <> h - 1 && Char.equal c.(i+1).[j] '.' then
+          Union_find.union uf.(i).(j) uf.(i+1).(j);
+        if j <> w - 1 && Char.equal c.(i).[j+1] '.' then
+          Union_find.union uf.(i).(j) uf.(i).(j+1)
+      end
+    done
+  done;
+
+  List.iter around ~f:(fun (sx, sy) ->
+      List.iter around ~f:(fun (ex, ey) ->
+          if (sx <> ex || sy <> ey) && Union_find.same_class uf.(sx).(sy) uf.(ex).(ey) then begin
+            print_endline "Yes";
+            exit 0
+          end
+        )
+    );
+
+  print_endline "No"
