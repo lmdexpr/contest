20697a62bf763968c1fe0b8318a09d71bf8e692b
lmdexpr <lmdexpr@gmail.com>
2023-02-17 17:09:56 +0900

    abc286 e

--- abc/abc286/e/e.ml
+++ abc/abc286/e/e.ml
@@ -1,11 +1,45 @@
 open Core
+open Scanf
 
-let _n = Scanf.scanf "%d" ident
+let n = scanf "%d" ident
 
-let _scan_1 _ = Scanf.scanf " %d" ident
-let _scan_2 _ = Scanf.scanf " %d %d" Tuple2.create
-let _scan_3 _ = Scanf.scanf " %d %d %d" Tuple3.create
+let a = Array.init n ~f:(fun _ -> scanf " %d" ident)
 
+let value = Array.make_matrix ~dimx:n ~dimy:n 0
+let dist  = Array.make_matrix ~dimx:n ~dimy:n n
 let () =
-  0
-  |> printf "%d\n%!"
+  for i = 0 to n - 1 do
+    dist.(i).(i) <- 0;
+    let s = scanf " %s" ident in
+    for j = 0 to n - 1 do
+      if Char.(s.[j] = 'Y') then (
+        dist.(i).(j)  <- 1;
+        value.(i).(j) <- a.(j)
+      )
+    done
+  done
+
+let _warshall_floyd =
+  for k = 0 to n - 1 do
+    for i = 0 to n - 1 do
+      for j = 0 to n - 1 do
+        let detour = dist.(i).(k) + dist.(k).(j) in
+        if detour = dist.(i).(j) then
+          value.(i).(j) <- max value.(i).(j) @@ value.(i).(k) + value.(k).(j)
+        else if detour < dist.(i).(j) then (
+          dist.(i).(j)  <- detour;
+          value.(i).(j) <- value.(i).(k) + value.(k).(j)
+        )
+      done
+    done
+  done
+
+let q = scanf " %d" ident
+let () =
+  for _ = 1 to q do
+    let u, v = scanf " %d %d" @@ fun u v -> u - 1, v - 1 in
+    if dist.(u).(v) = n then
+      printf "Impossible\n%!"
+    else
+      printf "%d %d\n%!" dist.(u).(v) (value.(u).(v) + a.(u))
+  done
