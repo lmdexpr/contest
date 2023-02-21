89a0f726f11111637d308408df7ea5c40e01af6d
lmdexpr <lmdexpr@gmail.com>
2023-02-21 19:55:28 +0900

    abc290 e

--- abc/abc290/e/e.ml
+++ abc/abc290/e/e.ml
@@ -3,12 +3,33 @@ open Scanf
 
 let n = scanf "%d" ident
 
-let _a = Array.init n ~f:(fun _ -> scanf " %d" ident)
+let p = Array.init (n+1) ~f:(const [])
+let () =
+  for i = 1 to n do
+    let a = scanf " %d" ident in
+    p.(a) <- Int64.of_int i :: p.(a)
+  done
 
-let _scan_1 _ = scanf " %d" ident
-let _scan_2 _ = scanf " %d %d" Tuple2.create
-let _scan_3 _ = scanf " %d %d %d" Tuple3.create
+let all =
+  Iter.(1 -- n)
+  |> Iter.map Int64.(fun i -> (of_int n + 1L - of_int i) * (of_int i / 2L))
+  |> Iter.fold Int64.(+) 0L
 
-let ans = 0
+let rec solve ?(acc=0L) p l r =
+  if l >= r then acc
+  else
+    let r_l = Int64.of_int (r - l) in
+    let pl = p.(l) and pr = Int64.(of_int n + 1L - p.(r)) in
+    if Int64.(pl < pr) then
+      solve p ~acc:Int64.(acc + r_l * pl) (l + 1) r
+    else 
+      solve p ~acc:Int64.(acc + r_l * pr) l (r - 1)
 
-let () = printf "%d\n%!" ans
+let same =
+  Iter.of_array p
+  |> Iter.map (fun p -> solve (Array.of_list_rev p) 0 (List.length p - 1))
+  |> Iter.fold Int64.(+) 0L
+
+let ans = Int64.(all - same)
+
+let () = printf "%Ld\n%!" ans
