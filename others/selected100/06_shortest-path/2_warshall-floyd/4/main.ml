306ee147bf15e4f82d594eb27e15625222fe3e0f
lmdexpr <lmdexpr@gmail.com>
2023-06-23 17:07:26 +0900

    complete warshall-floyd

--- others/selected100/06_shortest-path/2_warshall-floyd/4/main.ml
+++ others/selected100/06_shortest-path/2_warshall-floyd/4/main.ml
@@ -6,16 +6,27 @@ let n = scanf "%d" ident
 
 let a = Array.init n ~f:(fun _ -> Array.init n ~f:(fun _ -> scanf " %d" ident))
 
+let inf = 10_000_000_000
 let b = Array.init n ~f:(fun i -> Array.copy a.(i))
+let () = for i = 0 to n - 1 do b.(i).(i) <- inf done
+
 let _warshall_floyd =
-  for k = 1 to n do
-    for i = 1 to n do
-      for j = 1 to n do
-        b.(i).(j) <- min b.(i).(j) (b.(i).(k) + b.(k).(j))
+  for k = 0 to n - 1 do
+    for i = 0 to n - 1 do
+      for j = 0 to n - 1 do
+        if b.(i).(k) + b.(k).(j) < a.(i).(j) then begin
+          printf "-1\n%!";
+          exit 0
+        end
+        else if b.(i).(k) + b.(k).(j) = a.(i).(j) then 
+          b.(i).(j) <- inf
       done
     done
   done
   
-let ans = 0
+let filter x = if x < inf then x else 0
+
+let ans = Array.sum (module Int) b ~f:(Array.sum (module Int) ~f:filter)
+let ans = ans / 2
 
 let () = printf "%d\n%!" ans
