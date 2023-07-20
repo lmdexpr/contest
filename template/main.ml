6b5c272c71a093e560c45a16b66535a0d4ae6f53
lmdexpr <lmdexpr@gmail.com>
2023-07-20 01:04:18 +0000

    update to 5.0

--- template/main.ml
+++ template/main.ml
@@ -1,9 +1,9 @@
 open Core
 open Scanf
 
-let n = scanf "%d" ident
+let n = scanf "%d" Fn.id
 
-let _a = Array.init n ~f:(fun _ -> scanf " %d" ident)
+let _a = Array.init n ~f:(fun _ -> scanf " %d" Fn.id)
 
 let ans = 0
 
