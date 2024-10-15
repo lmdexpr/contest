602ce8af17d1acf1a9f944d19de2c8eb3fe87e3e
lmdexpr <lmdexpr@gmail.com>
2024-10-16 00:05:35 +0900

    tessoku-book

---
+++ others/tessoku-book/a01/a01.ml
@@ -0,0 +1,8 @@
+open Core
+open Scanf
+
+let n = scanf "%d" Fn.id
+
+let ans = n * n
+
+let () = printf "%d\n%!" ans
---
+++ others/tessoku-book/a14/a14.ml
@@ -0,0 +1,10 @@
+open Core
+open Scanf
+
+let n = scanf "%d" Fn.id
+
+let _a = Array.init n ~f:(fun _ -> scanf " %d" Fn.id)
+
+let ans = 0
+
+let () = printf "%d\n%!" ans
