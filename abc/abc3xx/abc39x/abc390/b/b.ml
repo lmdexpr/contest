d7d7e84034cf1ce6e54a59ffb0435e5edafa873e
lmdexpr <lmdexpr@gmail.com>
2025-01-26 17:27:28 +0900

    ratio

--- abc/abc3xx/abc39x/abc390/b/b.ml
+++ abc/abc3xx/abc39x/abc390/b/b.ml
@@ -1,19 +1,32 @@
 open Core
 open Scanf
 
+let rec gcd =function 
+  | 0, b -> b
+  | a, 0 -> a
+  | a, b -> gcd (if a < b then a, (b % a) else (a % b), b)
+let gcd = Tuple2.curry gcd
+
+module Ratio = struct
+  type t = { num : int; den : int }
+
+  let reduce { num; den } =
+    let g = gcd num den in
+    { num = num / g; den = den / g }
+
+  let (/) num den = reduce { num; den }
+
+  let (=) { num = num1; den = den1 } { num = num2; den = den2 } =
+    num1 = num2 && den1 = den2
+end
+
 let n = scanf " %d" Fn.id
 let a = Array.init n ~f:(fun _ -> scanf " %d" Fn.id)
 
-type ratio = { num : int; den : int }
-
-let compare r1 r2 =
-  let open Int64 in
-  compare (of_int r1.num * of_int r2.den) (of_int r2.num * of_int r1.den)
-
 let yes =
   Iter.(0 -- (n - 2))
-  |> Iter.map (fun i -> { num = a.(i + 1); den = a.(i) })
-  |> Iter.uniq ~eq:(fun r1 r2 -> compare r1 r2 = 0)
+  |> Iter.map Ratio.(fun i -> a.(i + 1) / a.(i))
+  |> Iter.uniq ~eq:Ratio.(=)
   |> Iter.length
   |> (=) 1
 
