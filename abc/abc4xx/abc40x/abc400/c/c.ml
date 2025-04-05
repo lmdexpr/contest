78d8dff1e50490f569e9cdb814cd5b799e7d12bf
lmdexpr <lmdexpr@gmail.com>
2025-04-05 23:46:45 +0900

    abc400 c upsolve

--- abc/abc4xx/abc40x/abc400/c/c.ml
+++ abc/abc4xx/abc40x/abc400/c/c.ml
@@ -1,22 +1,18 @@
 open Core
 open Scanf
 
-let n = scanf " %Ld" Fn.id
+let isqrt n =
+  let open Big_int in
+  big_int_of_int64 n
+  |> sqrt_big_int
+  |> int64_of_big_int
 
+let n = scanf " %Ld" Fn.id
 let ans =
   Iter.(1 -- 60)
-  |> Iter.scan Int64.(fun acc _ -> acc * 2L) 1L
-  |> Iter.drop 1
-  |> Iter.fold Int64.(fun acc a -> 
-    let b =
-      n / a |> Int64.to_float |> Float.sqrt |> Int64.of_float
-    in
-    let b =
-      if a * (b + 1L) * (b + 1L) <= n then b + 1L
-      else 
-        b
-    in
-    acc + if b % 2L = 0L then b / 2L else (b + 1L) / 2L
+  |> Iter.fold Int64.(fun acc a ->
+    let pow2 = shift_left 1L in
+    acc + (isqrt (n / pow2 a) + 1L) / 2L
   ) 0L
 
 let () = printf "%Ld\n%!" ans
