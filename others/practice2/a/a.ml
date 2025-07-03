e05cc39543922f1a618e01b391901ed721e93c3e
lmdexpr <lmdexpr@gmail.com>
2025-07-04 00:01:50 +0900

    uf

--- others/practice2/a/a.ml
+++ others/practice2/a/a.ml
@@ -1,10 +1,14 @@
 open Core
 open Scanf
 
-let n = scanf "%d" Fn.id
+let n = scanf " %d" Fn.id
+let q = scanf " %d" Fn.id
 
-let _a = Array.init n ~f:(fun _ -> scanf " %d" Fn.id)
+let dsu = Array.init n ~f:Union_find.create
 
-let ans = 0
-
-let () = printf "%d\n%!" ans
+let () =
+  for _ = 1 to q do
+    match scanf " %d %d %d" Tuple3.create with
+    | 0, u, v -> Union_find.union dsu.(u) dsu.(v)
+    | _, u, v -> printf "%d\n" @@ Bool.to_int Union_find.(same_class dsu.(u) dsu.(v))
+  done;
