ab26d23c0432f93fb8a0ec5b0e0cb3660fe1c40c
lmdexpr <lmdexpr@gmail.com>
2025-07-09 01:21:55 +0900

    abc413 d upsolve

--- abc/abc4xx/abc41x/abc413/d/d.ml
+++ abc/abc4xx/abc41x/abc413/d/d.ml
@@ -1,49 +1,25 @@
 open Core
 open Scanf
 
-let rec gcd =function 
-  | 0, b -> b
-  | a, 0 -> a
-  | a, b -> gcd (if a < b then a, (b % a) else (a % b), b)
-let gcd = Tuple2.curry gcd
-
-module Ratio = struct
-  type t = { sign : int; num : int; den : int }
-
-  let reduce { sign; num; den } =
-    let g = gcd num den in
-    { sign; num = num / g; den = den / g }
-
-  let (/) num den = 
-    let sign x = 
-      if x < 0 then -1
-      else if x > 0 then 1
-      else 0
-    in
-    reduce {
-      sign = sign num * sign den;
-      num = abs num;
-      den = abs den;
-    }
-
-  let (=)
-    { sign = sign1; num = num1; den = den1 }
-    { sign = sign2; num = num2; den = den2 }
-    =
-    sign1 = sign2 && num1 = num2 && den1 = den2
-end
+let yes n a =
+  Array.sort a ~compare:Int64.(fun x y -> compare (abs x) (abs y));
+  Iter.(0 -- (n - 3)) |> Iter.for_all (fun i ->
+    let i'  = Int.succ i in
+    let i'' = Int.succ i' in
+    Int64.(a.(i) * a.(i'') = a.(i') * a.(i'))
+  )
 
 let yes n a =
-  Array.sort a ~compare:(fun x y -> compare (abs x) (abs y));
-  Iter.(0 -- (n - 3)) |> Iter.for_all (fun i ->
-    Ratio.(a.(i) / a.(i + 1) = a.(i + 1) / a.(i + 2))
-  )
+  let pos_count = Array.count a ~f:Int64.(fun x -> x = a.(0)) in
+  let neg_count = Array.count a ~f:Int64.(fun x -> x = - a.(0)) in
+  (pos_count + neg_count = n && min pos_count neg_count = n / 2) ||
+  yes n a
 
 let t = scanf " %d" Fn.id
 
 let () =
   for _ = 1 to t do
     let n = scanf " %d" Fn.id in
-    let a = Array.init n ~f:(fun _ -> scanf " %d" Fn.id) in
+    let a = Array.init n ~f:(fun _ -> scanf " %Ld" Fn.id) in
     printf "%s\n%!" @@ if yes n a then "Yes" else "No"
   done
